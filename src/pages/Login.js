cf4b5d653837c563f426909b191338e701055797
Frederiek107 <f.a.tenbroeke@gmail.com>
2021-03-30 12:33:43 +0200

    latest changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,29 +4,30 @@ import {useForm} from "react-hook-form";
 import users from '../data/users.json'
 
 function Login({isAuthenticated, toggleIsAuthenticated}) {
-    const {handleSubmit, register}=useForm();
+    const {handleSubmit, register} = useForm();
 
     function onSubmit(data) {
-        console.log(data);
-    }
-
-    function handleClick() {
-        toggleIsAuthenticated(true);
+        for (const user of users) {
+            if (data.name === user.name) {
+                toggleIsAuthenticated(true);
+            }
+                }
     }
 
     return (
         <>
             <p>Dit is de inlogpagina. Klik op onderstaande knop om in te loggen.</p>
+            <p>Tip: bekijk het users.json-bestand om te zien met welke naam je kunt inloggen.</p>
             {!isAuthenticated &&
             <>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <label htmlFor="name" id="name">
                         Naam:
-                        <input ref={register} id="name"/>
+                        <input ref={register} type="text" id="name" name="name"/>
                     </label>
                 </form>
                 <button
-                    onClick={() => handleClick()}
+                    onClick={handleSubmit(onSubmit)}
                 >Log in
                 </button>
             </>}
